a4cfbe10345fbc9abd72b16d9b47192a07caae46
Jose Solano <jsolano@MacBook-Pro-de-crlocalad.local>
2025-08-21 17:31:02 -0600

    versionamiento de codigo

--- src/config.py
+++ src/config.py
@@ -3,15 +3,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GCP_PROJECT_ID = "connectdataai"
-LOCATION = "us-central1"
+# --- Lee la configuración desde las variables de entorno ---
+GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
+LOCATION = os.getenv("LOCATION")
 
 # --- CONFIGURACIÓN DE BIGQUERY ---
 BIGQUERY_DATASET_ID = "helpdesk_dex"
 TICKETS_TABLE_NAME = "tickets"
 EVENTOS_TABLE_NAME = "eventos_tiquetes"
 
-# --- CONFIGURACIÓN DEL HELPDESK (ASIGNACIÓN DE RESPONSABLES) ---
-###ESTO HAY QUE MOVERLO EVENTUALMENTE A UNA TABLA CON ROLES###
-DATA_ENGINEERING_LEAD = "jose.solano@connect.inc"
-BI_ANALYST_LEAD = "ivan.galindo@connect.inc"
+# --- CONFIGURACIÓN DEL HELPDESK ---
+DATA_ENGINEERING_LEAD = os.getenv("DATA_ENGINEERING_LEAD", "jose.solano@connect.inc")
+BI_ANALYST_LEAD = os.getenv("BI_ANALYST_LEAD", "ivan.galindo@connect.inc")
